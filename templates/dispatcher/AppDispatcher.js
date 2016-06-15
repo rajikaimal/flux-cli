fd6ff8a2b96b717f26ddda8c033428a13fe52b11
rajikaimal <inbox.rajika@gmail.com>
2016-06-15 23:12:36 +0530

    dispatcher provided by facebook (c)

---
+++ templates/dispatcher/AppDispatcher.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * AppDispatcher
+ *
+ * A singleton that operates as the central hub for application updates.
+ */
+
+var Dispatcher = require('flux').Dispatcher;
+var assign = require('object-assign');
+
+var AppDispatcher = assign(new Dispatcher(), {
+  handleViewAction: function (action) {
+    this.dispatch({
+      source: 'VIEW_ACTION',
+      action: action,
+    });
+  },
+});
+
+module.exports = AppDispatcher;
