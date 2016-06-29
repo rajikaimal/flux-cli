c9631f7e13b3156512488823c2967bc7c0c93079
rajikaimal <inbox.rajika@gmail.com>
2016-06-29 22:48:35 +0530

    add chalk

--- bin/flux.js
+++ bin/flux.js
@@ -1,74 +1,68 @@
 #!/usr/bin/env node
 var program = require('commander');
+var chalk = require('chalk');
 var version = require('../package.json').version;
 var init = require('../lib/init');
 var initType = require('../lib/type');
+
+var clr = chalk.bold.cyan;
 
 program
   .command('init <appname>')
   .alias('i')
   .description('initialize flux application')
   .action(function(appname){
-    console.log('---Initializing flux application: %s', appname);
+    var initializeClr = chalk.bold.yellow;
+    console.log(initializeClr('Initializing flux application: %s'), appname);
     init(appname);
-  }).on('--help', function() {
-    
   });
 
 program
   .command('action <name>')
   .alias('a')
-  .description('initialize flux application')
+  .description('create action file')
   .action(function(name){
-    console.log('---creating action: %s', name);
+    console.log(clr('Creating action: %s'), name);
     var argArr = name.split(':');
     var module = argArr[0];
     var actionName = argArr[1];
     initType('action', module, actionName);
-  }).on('--help', function() {
-  
   });
 
 program
   .command('component <name>')
   .alias('c')
-  .description('initialize flux application')
+  .description('create component file')
   .action(function(name){
-    console.log('---creating component: %s', name);
+    console.log(clr('Creating component: %s'), name);
     var argArr = name.split(':');
     var module = argArr[0];
     var actionName = argArr[1];
     initType('component', module, actionName);    
-  }).on('--help', function() {
- 
   });
 
 program
   .command('store <name>')
   .alias('s')
-  .description('initialize flux application')
+  .description('create store file')
   .action(function(name){
-    console.log('---creating store: %s', name);
+    console.log(clr('Creating store: %s'), name);
     var argArr = name.split(':');
     var module = argArr[0];
     var actionName = argArr[1];
     initType('store', module, actionName);
-  }).on('--help', function() {
- 	
   });
 
 program
   .command('constant <name>')
   .alias('k')
-  .description('initialize flux application')
+  .description('create constant file')
   .action(function(name){
-    console.log('---creating constant: %s', name);
+    console.log(clr('Creating constant: %s'), name);
     var argArr = name.split(':');
     var module = argArr[0];
     var actionName = argArr[1];
     initType('constant', module, actionName);
-  }).on('--help', function() {
-  
   });
 
 
