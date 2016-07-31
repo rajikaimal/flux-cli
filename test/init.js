d72979ff2823566397bd891defbf68ff22a17fd0
rajikaimal <inbox.rajika@gmail.com>
2016-07-31 23:49:31 +0530

    init test

--- test/init.js
+++ test/init.js
@@ -1,1 +1,18 @@
 var mocha = require('mocha');
+var init = require('../lib/init');
+var fs = require('fs');
+
+describe('Initialize application', function() {
+    it('should initialize application', function(done) {
+    	init('mobx');
+    	fs.stat('../mobx', function(err, buff) {
+    		if(err) throw new Error('error ...');
+    		
+    		fs.rmdir('../mobx', function(err, status) {
+    			if(err) throw new Error('error ...');
+
+    			done();
+    		});
+    	});
+    });
+});
